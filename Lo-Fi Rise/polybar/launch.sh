796a4367f522242133abc5761cad0914821d0a2a
BrutalWizard <minerdeath62@gmail.com>
2021-03-04 01:17:21 +0300

    Fixed StartUp

--- Lo-Fi Rise/polybar/launch.sh
+++ Lo-Fi Rise/polybar/launch.sh
@@ -9,8 +9,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch polybar
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar main -c $(dirname $0)/config.ini &
+        MONITOR=$m polybar main &
     done
 else
-    polybar main -c $(dirname $0)/config.ini &
+    polybar main &
 fi
