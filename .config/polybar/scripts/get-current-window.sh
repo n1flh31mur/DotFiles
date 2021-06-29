4eb3958736bcb9c07f9011549405f402cb913e2a
BrutalWizard <minerdeath62@gmail.com>
2021-06-29 21:24:15 +0300

    Updated polybar

--- .config/polybar/scripts/get-current-window.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xdotool getwindowfocus getwindowname
---
+++ .config/polybar/scripts/launcher.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FILE="$HOME/.config/polybar/scripts/rofi/colors.rasi"
+
+rofi -no-config -no-lazy-grab -show-icons -show drun -modi drun -theme ~/.config/polybar/scripts/rofi/launcher.rasi
