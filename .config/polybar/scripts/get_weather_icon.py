1490bc42feb612095eb1cd1332109cba6d9539f2
BrutalWizard <bru74lw1z4rd@gmail.com>
2022-10-08 12:29:57 +0300

    Changed weather Font Awesome to Nerd Font

--- .config/polybar/scripts/get_weather_icon.py
+++ .config/polybar/scripts/get_weather_icon.py
@@ -19,37 +19,37 @@ temperature_unit = "C" # Units of measurement. That will be showed in UI. Does n
 
 
 icons_list = {
-    "01d": "", # Clear sky day.
-    "01n": "", # Clear sky night.
-    "02d": "", # Few clouds day.
-    "02n": "", # Few clouds night.
-    "03d": "", # Scattered clouds day.
-    "03n": "", # Scattered clouds night.
-    "04d": "", # Broken clouds day.
-    "04n": "", # Broken clouds night.
-    "09d": "", # Shower rain day.
-    "09n": "", # Shower rain night.
-    "10d": "", # Rain day.
-    "10n": "", # Rain night
-    "11d": "", # Thunderstorm day.
-    "11n": "", # Thunderstorm night
-    "13d": "", # Snow day. Snowflake alternative: 
-    "13n": "", # Snow night. Snowflake alternative: 
-    "50d": "", # Mist day.
-    "50n": "", # Mist night.
+    "01d": "", # Clear sky day.
+    "01n": "望", # Clear sky night.
+    "02d": "", # Few clouds day.
+    "02n": "", # Few clouds night.
+    "03d": "", # Scattered clouds day.
+    "03n": "", # Scattered clouds night.
+    "04d": "", # Broken clouds day.
+    "04n": "", # Broken clouds night.
+    "09d": "歹", # Shower rain day.
+    "09n": "歹", # Shower rain night.
+    "10d": "", # Rain day.
+    "10n": "", # Rain night
+    "11d": "", # Thunderstorm day.
+    "11n": "", # Thunderstorm night
+    "13d": "", # Snow day. Snowflake alternative: 
+    "13n": "", # Snow night. Snowflake alternative: 
+    "50d": "", # Mist day.
+    "50n": "", # Mist night.
 }
 
 atmophere_icons_list = {
-    701: "", # Mist
-    711: "", # Smoke
-    721: "", # Haze
-    731: "", # Dust (Sand / dust whirls)
-    741: "", # Fog
-    751: "", # Sand
-    761: "", # Dust
-    762: "", # Ash
-    771: "", # Squalls
-    781: ""  # Tornado
+    701: "", # Mist
+    711: "", # Smoke
+    721: "", # Haze
+    731: "", # Dust (Sand / dust whirls)
+    741: "", # Fog
+    751: "", # Sand
+    761: "", # Dust
+    762: "", # Ash
+    771: "", # Squalls
+    781: ""  # Tornado
 }
 
 
